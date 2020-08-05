d8bcc02c60a9753ce9c1d74c68b10f3c1db67fe3
Michael Rabon <nobar86@gmail.com>
2020-08-05 17:12:05 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -37,7 +37,7 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
-     programmer_hash[:known_for]
+     programmer_hash[:known_for, "Object Orientation"]
 end
 
 def dennis_ritchies_language
